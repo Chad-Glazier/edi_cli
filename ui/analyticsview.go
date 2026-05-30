a9090ffe51b3529bf85a731e8abf25f6544fe833
Chad Glazier <chadglazier@outlook.com>
2026-05-29 19:26:35 -0700

    formatting

--- ui/analyticsview.go
+++ ui/analyticsview.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Returns a formatted string representation of a VI's analytics. If the VI
-// yields unsupported analytics, an error is returned. 
+// yields unsupported analytics, an error is returned.
 func AnalyticsView(vi edi.VI) (string, error) {
 	switch a := vi.GetAnalytics().(type) {
 	case edi.EDIAnalytics:
@@ -21,15 +21,10 @@ func AnalyticsView(vi edi.VI) (string, error) {
 	}
 }
 
-
 //
 // Single search alpha-beta analytics.
 //
 
-
-
 //
 // Aggregate alpha-beta analytics.
 //
-
-
